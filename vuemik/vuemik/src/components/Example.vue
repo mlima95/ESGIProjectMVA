a37c99e77bde2b60298991b36d2dc9c68602d81f
JACQUENET Christophe <jjacquenet@myges.fr>
2022-02-15 15:29:47 +0100

    save

---
+++ vuemik/vuemik/src/components/Example.vue
@@ -0,0 +1,21 @@
+<template>
+  <Vuemik
+    :initial-values=""
+
+    >
+
+  </Vuemik>
+
+
+</template>
+
+<script>
+import Vuemik from './Vuemik.vue';
+
+export default {
+  name: 'Example',
+  components: {
+    Vuemik
+  }
+}
+</script>
---
+++ vuemik/vuemik/src/components/Vuemik.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Vuemik',
+  props: {
+    initialValues: {
+      type: Object,
+      required: true
+    },
+    validation: {
+      type: Object,
+      required: false
+    }
+  },
+  methods: {
+    onSubmit(){
+
+    }
+  }
+}
+</script>
